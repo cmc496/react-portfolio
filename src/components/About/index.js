793df55617b2de3ca8d7e2c9004f828ac36f7179
Agatha Harkness <caitlinmarie.clifford@gmail.com>
2022-08-07 22:48:26 -0400

    About complete

---
+++ src/components/About/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function About() {
+    return (
+        <div>
+            <p>Currently studying to become a full-stack web developer.</p>
+
+            <p>Hello! My name is Caitlin Clifford and I am currently enrolled in a full-stack web development program.</p>
+
+            <p>My favorite part of coding so far is definitely styling with CSS! 
+                I am proud of the projects I've styled thus far, 
+                and I look forward to getting even better at it.</p>
+        </div>
+    );
+}
+
+export default About;
