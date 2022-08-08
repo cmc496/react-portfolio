bb9fa59d013f7b2f196c8110978aa5e01b404f91
Agatha Harkness <caitlinmarie.clifford@gmail.com>
2022-08-08 16:56:30 -0400

    Resume index.js complete

---
+++ src/components/Resume/index.js
@@ -0,0 +1,26 @@
+import react from 'react';
+function Resume () {
+    return (
+        <div>
+            <div>
+                <p>Resume</p>
+
+                <a href="">
+                <span className='icon'>
+                    <i></i>
+                </span>
+                <span>Download my Resume!</span>
+                </a>
+            </div>
+            <div>
+                <p>Skills:</p>
+                <ul>
+                    <li>JavaScript, HTML, CSS, Markdown</li>
+                    <li>Node.js, MYSQL, MongoDB, React</li>
+                    <li>Git, GitHub</li>
+                </ul>
+            </div>
+        </div>
+    );
+}
+export default Resume;
