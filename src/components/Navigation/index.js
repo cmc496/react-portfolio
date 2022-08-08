afc3b03cfc51159daafda49b277d507a8714615e
Agatha Harkness <caitlinmarie.clifford@gmail.com>
2022-08-08 16:08:12 -0400

    Navigation index.js complete

---
+++ src/components/Navigation/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Navigation (props) {
+    const tabs = ["About", "Portfolio", "Contact", "Resume"];
+    return (
+        <div>
+            <ul>{tabs.map((tab)=> (
+                <li className={props.currentPage === tab ? "nav-item is-active" : "nav-item"}
+                key={tab}>
+                    <a href={"#" + tab.toLowerCase()} onClick={() => props.handlePageChange(tab)} className={props.currentPage === tab ? "nav-link active" : "nav-link"}>
+                        {tab}
+                    </a>
+                </li>
+            ))}</ul>
+        </div>
+    );
+}
+export default Navigation;
