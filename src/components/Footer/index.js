fcf6d64fa7ff90d3c0111707c615d261007c98ae
Agatha Harkness <caitlinmarie.clifford@gmail.com>
2022-08-08 15:57:21 -0400

    footer index.js complete

---
+++ src/components/Footer/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+function Footer () {
+    return (
+        <footer className='footer'>
+            <div>
+                <p>
+                    <h1>Full Stack Portfolio</h1> - React website made by{" "}
+                    <a href='https://github.com/cmc496'>Caitlin Clifford</a>.
+
+                    <a href="https://www.linkedin.com/in/caitlin-clifford-358732115/">LinkedIn</a>{" "}
+                </p>
+            </div>
+        </footer>
+    );
+}
+export default Footer;
